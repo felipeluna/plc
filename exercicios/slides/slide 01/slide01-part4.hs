0da0ed9aa9682c991efbc3bf721b2475b6db4191
Felipe Luna <felipe@tardis.home>
2015-08-31 21:02:49 -0300

    ordenar numeros a partir da soma dos digitos

--- exercicios/slides/slide 01/slide01-part4.hs
+++ exercicios/slides/slide 01/slide01-part4.hs
@@ -1,10 +1,18 @@
 
+-- Crie um função que recebe uma lista de inteiros e
+-- retorna a lista ordenada em função da soma de seus 
+-- digitos(crescente):
 sumDigits :: Int -> Int
 sumDigits a = (div a 10) + (mod a 10)
 
 sumDigitsTupla :: Int -> (Int, Int)
 sumDigitsTupla a = (a,sumDigits(a))
 
-sortTupla :: [(Int, Int)] -> [Int]
-sortTupla [] = []
 
+sortTupla :: [(Int, Int)] -> [(Int, Int)]
+sortTupla [] = []
+sortTupla (x:xs) =  sortTupla [b | b <- xs, snd(b) < snd(x)] ++ [x] ++ sortTupla[b | b <- xs, snd(b) >= snd(x)]
+
+ordenar :: [Int] -> [Int]
+ordenar [] = []
+ordenar a = map fst $ sortTupla $ map sumDigitsTupla a
