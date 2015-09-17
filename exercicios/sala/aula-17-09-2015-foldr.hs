0f3c41218f2636085cb345f68dc6d9637483eba6
Felipe Santana de Luna <fsl3@ug1c09.windows.cin.ufpe.br>
2015-09-17 15:46:01 -0300

    aprendendno acumuladores, media feita de varias maneiras

---
+++ exercicios/sala/aula-17-09-2015-foldr.hs
@@ -0,0 +1,39 @@
+foldr' op id [] = id
+foldr' op id (x:xs) = op x (foldr' op id xs)
+
+-- media com acumuladores
+media s t [] = s/t
+media s t (x:xs) = media (s + x) (t + 1) xs
+
+-- sum com acumuladores
+sum' a [] = a
+sum' a (x:xs) = sum' (a+x) xs
+
+-- product com acumuladores
+product' a [] = a
+product' a (x:xs) = product' (a*x) xs
+
+
+-- media com acumuladores
+
+
+summ (a,b) [] = (a,b)
+summ (a,b) (x:xs) = summ ((a+x),(b+1)) xs
+
+
+med l = s/t
+	where (s,t) =  summ (0,0) l	
+
+-- media com foldr
+
+summ' 0 (a,b) = (a, b)
+summ' x (a,b) = (a+x, b+1)
+
+medd' l = s/t
+	where (s,t) = foldr summ' (0,0) l
+
+-- media com lambda
+
+medd l = s/t
+	where (s,t) = foldr (\x (s,t)-> (s+x,t+1)) (0,0) l
+
