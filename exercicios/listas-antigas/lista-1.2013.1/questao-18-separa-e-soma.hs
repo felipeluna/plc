fe0221c915e2dd95dabdd03354aec2c874418ff0
Felipe Luna <fsl3@cin.ufpe.br>
2015-09-24 11:10:23 -0700

    questao 18 feita. andando duas vezes na lista fazer com acumuladores andando uma vez sem e com acumuladores

--- exercicios/listas-antigas/lista-1.2013.1/questao-18-separa-e-soma.hs
+++ exercicios/listas-antigas/lista-1.2013.1/questao-18-separa-e-soma.hs
@@ -1,6 +1,21 @@
-
-
 separaEsoma [] = (0,0)
-separaEsoma lista = (folderl (+) par, folderl (+) impar)
-                     where par = filter (mod 2 == 0 lista
-                           impar = filter (mod 2 /= 0) lista
+separaEsoma lista = (foldr1 (+) par, foldr1 (+) impar)
+                     where par = filter even lista
+                           impar = filter odd lista
+                           
+separaEsoma2 [] = (0,0)
+separaEsoma2 lista = (foldr1 (+) par, foldr1 (+) impar)
+                     where (par, impar) = separar lista 
+
+separaEsoma3 [] = (0,0)
+separaEsoma3 lista = (foldr1 (+) par, foldr1 (+) impar)
+                     where (par, impar) = separar2 lista ([],[])
+                           
+-- separar primeira maneira					 
+separar [] = ([],[])
+separar (x:xs) = if even x then (x:left, right) else (left, x:right)
+				 where (left, right) = separar xs
+
+-- separar com acumulador				 
+separar2 [] (a,b) = (a,b)
+separar2 (x:xs) (a,b) = if even x then separar2 xs (x:a,b) else separar2 xs (a, x:b) 
