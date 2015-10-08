27554b2d8359b7a5ecc55a4bae30c60fc358d358
Felipe Santana de Luna <fsl3@ug4c10.windows.cin.ufpe.br>
2015-10-08 11:46:57 -0300

    questao 2 e 3, duvida se a 3 eh so isso ou tem mais coisa

--- exercicios/provas/ee1-2014-2/prova.hs
+++ exercicios/provas/ee1-2014-2/prova.hs
@@ -16,3 +16,28 @@ lookupp s [] = Erro
 lookupp s ((ss,vv):xs) = if s == ss then vv else lookupp s xs
 
 
+-- questao 2
+-- estudar instance 
+
+instance Eq Valor where
+	 (Num a) == (Num b) = a == b
+	 (Bool a) == (Bool b) = a == b
+	 Erro == Erro = True
+	 (Fun a) == (Fun b) = a == b -- duvida
+         _==_ = False -- duvida
+
+-- questao 3
+
+data ExpB = V | F | NEG ExpB | OU ExpB ExpB | VarB Id | AplB Operador ExpI
+data ExpI = Lit Int | VarI Id | Apl Operador ExpI
+
+type Operador = String
+type Id = String
+
+data Comando =    Atribuicao Id ExpI
+		| Condicional ExpB Comando Comando 
+                | While ExpB Comando
+ 		| Composicao Comando Comando	
+
+     
+
