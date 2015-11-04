a9d91843aefeabcbce020e30886afe3f3bc36afc
Felipe Luna <felipe@tardis.home>
2015-11-03 21:55:08 -0300

    agora ta dando join

--- concorrencia/exercicios-slide/Primos.java
+++ concorrencia/exercicios-slide/Primos.java
@@ -38,7 +38,8 @@ public class Primos extends Thread
         int a = maxPrimos/qntThread;
         int min = 2;
         int max;
-        for(int i = 1; i <= qntThread; i++)
+        Thread [] ts = new Thread[qntThread];
+        for(int i = 0; i < qntThread; i++)
         {
             max = min + a;
             if(max > maxPrimos)
@@ -47,9 +48,23 @@ public class Primos extends Thread
             }
             //System.out.print(min + " " + max + "\n");
 
-            (new Primos(min,max)).start();
+            ts[i] = new Primos(min,max);
             min = max + 1;
+            ts[i].start();
         } 
+         for(int i = 0; i < qntThread; i++)
+        {
+            try 
+            {
+                ts[i].join();
+            } 
+            catch (InterruptedException ie)
+            {
+             Thread.currentThread().interrupt();  // set interrupt flag
+             System.out.println("Failed to thread");
+            }
+        }
+        
         System.out.println("Jonas");
     }
 }
