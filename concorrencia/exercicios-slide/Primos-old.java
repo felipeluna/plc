58c9498a309bcd4a7fbfc0b22106309e2c1baae8
Felipe Santana de Luna <fsl3@ug4c42.windows.cin.ufpe.br>
2015-11-16 15:04:11 -0300

    errado

---
+++ concorrencia/exercicios-slide/Primos-old.java
@@ -0,0 +1,73 @@
+/*Implemente um programa que calcula todos os números primos entre 1 e um valor N fornecido como argumento. Seu programa deve dividir o trabalho a ser realizado entre X threads (onde X também é uma entrada do programa) para tentar realizar o trabalho de maneira mais rápida que uma versão puramente sequencial.
+A thread principal do programa deve imprimir os números primos identificados apenas quando as outras threads terminarem. */
+
+public class Primos extends Thread
+{
+    int min;
+    int max;
+    public Primos (int min, int max)
+    {
+        this.min = min;
+        this.max = max; 
+    }
+
+    public void run()
+    {
+        for(int i = min; i <= max; i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.println(i); 
+            } 
+        } 
+    }
+    
+    public boolean isPrime(int n)
+    {
+        for(int i = 2; i < n; i++)
+        {
+           if(n%i == 0)
+           {
+               return false;
+           } 
+        }
+        return true;
+    } 
+
+    public static void main (String[] args)
+    {
+        int maxPrimos = 70;
+        int qntThread = 5;
+        int a = maxPrimos/qntThread;
+        int min = 2;
+        int max;
+        Thread [] ts = new Thread[qntThread];
+        for(int i = 0; i < qntThread; i++)
+        {
+            max = min + a;
+            if(max > maxPrimos)
+            { 
+                max = maxPrimos; 
+            }
+            //System.out.print(min + " " + max + "\n");
+
+            ts[i] = new Primos(min,max);
+            min = max + 1;
+            ts[i].start();
+        } 
+         for(int i = 0; i < qntThread; i++)
+        {
+            try 
+            {
+                ts[i].join();
+            } 
+            catch (InterruptedException ie)
+            {
+             Thread.currentThread().interrupt();  // set interrupt flag
+             System.out.println("Failed to thread");
+            }
+        }
+        
+        System.out.println("Main");
+    }
+}
--- concorrencia/exercicios-slide/Primos.java
+++ concorrencia/exercicios-slide/Primos.java
@@ -1,28 +1,22 @@
-/*Implemente um programa que calcula todos os números primos entre 1 e um valor N fornecido como argumento. Seu programa deve dividir o trabalho a ser realizado entre X threads (onde X também é uma entrada do programa) para tentar realizar o trabalho de maneira mais rápida que uma versão puramente sequencial.
-A thread principal do programa deve imprimir os números primos identificados apenas quando as outras threads terminarem. */
-
-public class Primos extends Thread
+import java.util.ArrayList;
+public class Primos
 {
-    int min;
-    int max;
-    public Primos (int min, int max)
-    {
-        this.min = min;
-        this.max = max; 
-    }
 
-    public void run()
+    static int count = 1;
+    static ArrayList<Integer> lista = new ArrayList<Integer>();
+    
+    public static synchronized int getCounter()
     {
-        for(int i = min; i <= max; i++)
-        {
-            if(isPrime(i))
-            {
-                System.out.println(i); 
-            } 
-        } 
+        count = count + 1;
+        return count;
     }
     
-    public boolean isPrime(int n)
+    public static synchronized void addList(int num)
+    {
+        lista.add(num);
+    }
+    
+    public static boolean isPrime(int n)
     {
         for(int i = 2; i < n; i++)
         {
@@ -33,41 +27,78 @@ public class Primos extends Thread
         }
         return true;
     } 
-
-    public static void main (String[] args)
+    
+    public static void main(String [] args) throws Exception    
     {
-        int maxPrimos = 70;
-        int qntThread = 5;
-        int a = maxPrimos/qntThread;
-        int min = 2;
-        int max;
-        Thread [] ts = new Thread[qntThread];
-        for(int i = 0; i < qntThread; i++)
-        {
-            max = min + a;
-            if(max > maxPrimos)
-            { 
-                max = maxPrimos; 
-            }
-            //System.out.print(min + " " + max + "\n");
-
-            ts[i] = new Primos(min,max);
-            min = max + 1;
-            ts[i].start();
-        } 
-         for(int i = 0; i < qntThread; i++)
-        {
-            try 
-            {
-                ts[i].join();
-            } 
-            catch (InterruptedException ie)
-            {
-             Thread.currentThread().interrupt();  // set interrupt flag
-             System.out.println("Failed to thread");
-            }
-        }
+ 
+        int max = 1000000;
+        Work t1 = new Work(max);
+        Work t2 = new Work(max);
+        Work t3 = new Work(max);
+        Work t4 = new Work(max);
+        Work t5 = new Work(max);
+        Work t6 = new Work(max);
+        Work t7 = new Work(max);
+        Work t8 = new Work(max);
+        Work t9 = new Work(max);
+        Work t10 = new Work(max);
+        Work t11 = new Work(max);
         
-        System.out.println("Main");
+        
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
+        t8.start();
+        t9.start();
+        t10.start();
+        t11.start();
+        
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        t6.join();
+        t7.join();
+        t8.join();
+        t9.join();
+        t10.join();
+        t11.join();
+        
+        for(int i = 0; i < lista.size(); i++)
+        {
+            System.out.println(lista.get(i));
+        }
     }
+
 }
+    class Work extends Thread
+    {
+         int max;
+
+         public Work(int max)
+         {
+            this.max = max;
+ 
+         }
+         
+         public void run()
+         {
+             int count = Primos.getCounter();
+             while(count < max)
+             {
+                if(Primos.isPrime(count))
+                {
+
+                    Primos.addList(count);
+                }
+
+                count = Primos.getCounter();
+             }
+         }
+    }
+
