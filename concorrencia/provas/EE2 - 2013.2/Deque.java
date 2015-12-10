8be19f0b9611ddf696147f0d1b13f004e6740585
Felipe Santana de Luna <fsl3@ug3c38.windows.cin.ufpe.br>
2015-12-10 09:52:33 -0300

    asdfhiasdfhi

--- concorrencia/provas/EE2 - 2013.2/Deque.java
+++ concorrencia/provas/EE2 - 2013.2/Deque.java
@@ -1,11 +1,12 @@
 import java.util.LinkedList;
+import java.util.concurrent.locks.*;
 class Deque
 {
     LinkedList<Integer> lista = new LinkedList<Integer>();
     Lock lesq = new ReentrantLock(true);
     Lock ldir = new ReentrantLock(true);
     Lock vazia = new ReentrantLock(true);
-
+    Condition condVazia = vazia.newCondition();
     
     public void push_left(int i)
     {
@@ -17,6 +18,7 @@ class Deque
            {
                if(lista.size() == 0)
                {
+                   System.out.println(i + " entrou numa lista vazia");
                    lista.addFirst(i);
                }
            }
@@ -24,7 +26,36 @@ class Deque
            {
                 vazia.unlock(); 
            }
-           lista.addFirst(i); 
+
+           condVazia.signalAll(); 
+        }
+        finally
+        {
+            lesq.unlock();
+        }
+    }
+    public void pop_left()
+    {
+        lesq.lock();
+        try
+        {
+           vazia.lock();
+           try
+           {
+               while(lista.size() == 0)
+               {
+                    try
+                    {
+                        condVazia.await();    
+                    }catch(InterruptedException e) {}
+               }
+           }
+           finally
+           {
+                vazia.unlock(); 
+           }
+           
+           lista.removeFirst(); 
         }
         finally
         {
@@ -32,4 +63,43 @@ class Deque
         }
     }
     
+    public static void main(String [] args)
+    {
+        Deque d = new Deque();
+        prod p = new prod(d,4);
+        cons c = new cons(d);
+        p.start();
+        c.start();
+    }
+    
+}
+
+
+class prod extends Thread
+{
+    Deque d;
+    int i;
+    public prod(Deque d, int i)
+    {
+        this.d = d;
+        this.i = i;
+    }      
+    public void run()
+    {
+        d.push_left(i);   
+    }
+}
+
+class cons extends Thread
+{
+    Deque d;
+
+    public cons(Deque d)
+    {
+        this.d = d;
+    }      
+    public void run()
+    {
+        d.pop_left();   
+    }
 }
